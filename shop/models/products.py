179fbe7a6904ed3d6e11d3e2b16f8ccb88aedfc9
sasha <hello@gmail.com>
2025-02-04 20:09:37 +0200

    first commit

--- shop/models/products.py
+++ shop/models/products.py
@@ -1,10 +1,14 @@
 from django.db import models
-
+from shop.managers.products import ProductManager
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    objects = ProductManager()
+
+
 
 
 class Article(models.Model):
